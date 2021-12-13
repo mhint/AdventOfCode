7b80b2c044a914f59345239d9b67be318863f2d4
Morris Hinton <mhinton294@gmail.com>
2021-12-13 01:38:20 -0800

    Update puzzle solutions

--- src/01/star1.ts
+++ src/01/star1.ts
@@ -3,18 +3,15 @@ import * as readline from 'readline';
 
 const inputFile: string = './static/01/input';
 
-/* Part One: Count the number of times a depth measurement increases from the previous measurement */
-
+/* Count the number of times a depth measurement increases from the previous measurement */
 async function timesIncreased(file: string): Promise<void> {
     const rl = readline.createInterface({
         input: fs.createReadStream(file)
     });
-
     let depthIncreases: number = 0;
     let depthCount: number = 0;
     let curDepth: any = undefined;
     let prevDepth: any = undefined;
-
     for await (const depth of rl) {
         curDepth = Number(depth);
         if (curDepth != undefined && prevDepth != undefined) {
--- src/01/star2.ts
+++ src/01/star2.ts
@@ -3,19 +3,16 @@ import * as readline from 'readline';
 
 const inputFile: string = './static/01/input';
 
-/* Part Two: Count the number of times the sum of measurements in this sliding window increases from the previous sum */
-
+/* Count the number of times the sum of measurements in this sliding window increases from the previous sum */
 async function timesSumsIncreased(file: string): Promise<void> {
     const rl = readline.createInterface({
         input: fs.createReadStream(file)
     });
-
     let depthSumIncreases: number = 0;
     let depthCount: number = 0;
     let curDepths: [any, any, any] = [undefined, undefined, undefined];
     let curDepthSum: any = undefined;
     let prevDepthSum: any = undefined;
-
     for await (const depth of rl) {
         curDepths[0] = curDepths[1];
         curDepths[1] = curDepths[2];
@@ -29,7 +26,6 @@ async function timesSumsIncreased(file: string): Promise<void> {
         prevDepthSum = curDepthSum;
         depthCount++;
     };
-
     console.log(`${depthSumIncreases} sums are larger than the previous sum.`);
 }
 
